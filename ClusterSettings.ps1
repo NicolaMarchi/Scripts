d4ea8dd2b48230b73cce7a7f7be08ce0259a6d4d
NicolaMarchi <nicola.marchi@gmail.com>
2017-06-06 11:04:46 +0200

    Add files via upload

---
+++ ClusterSettings.ps1
@@ -0,0 +1,12 @@
+﻿#Restituisce i Valori configurati
+$cluster=Get-Cluster -Name sqlmossprclu
+Write-Host $cluster.name
+Write-Host 'Valori configurati prima della modifica:'
+get-cluster -Name SQLMRPR02CLU | fl *subnet*
+$cluster.SameSubnetDelay = 2000
+$cluster.CrossSubnetThreshold = 10
+$cluster.CrossSubnetDelay = 2000
+$cluster.SameSubnetThreshold = 10
+Write-Host $cluster.name
+Write-Host 'Valori configurati dopo la modifica:'
+get-cluster -Name pspr01 | fl *subnet*
